a442146eca15bdc8c3e2bcb41ba50fb0b73b4eb4
Diego Seoane <a22diegosg@iessanclemente.net>
2023-02-14 18:01:22 +0100

    a

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,15 +11,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        /* System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://es.wikipedia.org/"); 
+            
                     
             
             WebElement cajaBusqueda = driver.findElement(By.id("searchInput"));
-            cajaBusqueda.sendKeys("Selenium");
-            System.out.println(driver.getTitle());
+            cajaBusqueda.sendKeys("Taragoña");
+            WebElement botonBusqueda = driver.findElement(By.id("searchButton"));
+            botonBusqueda.click();
+
+            assertEquals("Taragoña - Wikipedia, la enciclopedia libre",driver.getTitle());            
+
+            //System.out.println(driver.getTitle());
+            driver.findElement(By.id("n-portal")).click();
+            System.out.println(driver.findElement(By.id("fristHeading")));
+            System.out.println(tituloPagina.getTitle());
+            assertEquals("Portal:Comunidad", cajaBusqueda);
+            
+            /*System.out.println(driver.findElement(By.id("footer")).getAttribute("role"));
+
+            List<WebElement> elementosDiv = driver.findElements(By.tagName("div"));
+            for (WebElement webElement : elementosDiv) {
+                System.out.println(webElement.getAttribute("innerText"));
+            }
             Thread.sleep(3000); 
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
@@ -22,6 +44,17 @@ public class App {
         }
         
         driver.quit();
+        */
+
+        
     }
+    @Test
+        public void test_checkbox(){
+            System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.get("src/index.html");
+        }
+
+        
     
 }
